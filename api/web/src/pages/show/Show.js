8a121ae00ef4369ef7160d672e609843e1b62018
N7-n <nn8564986@gmail.com>
2023-04-24 19:49:20 +0900

    fix

--- api/web/src/pages/show/Show.js
+++ api/web/src/pages/show/Show.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Block from './components/block/Block'
+//import Block from './components/block/Block'
 
 function Show(){
   const [store, setStore] = useState("");
